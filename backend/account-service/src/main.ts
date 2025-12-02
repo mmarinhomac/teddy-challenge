fa5424f2a33ad23b796478d75d850b119fccf385
Matheus Marinho <mmarinhomac@gmail.com>
2025-12-02 18:41:36 -0300

    ignore .env and cors handler

--- backend/account-service/src/main.ts
+++ backend/account-service/src/main.ts
@@ -1,14 +1,25 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+
+import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn'],
   });
+  const configService = app.get(ConfigService);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  const allowedOrigins = configService.get<string>('CORS_ORIGIN')?.split(',');
+
+  app.enableCors({
+    origin: allowedOrigins, // Usa a lista de origens
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Teddy Open Finance API')
